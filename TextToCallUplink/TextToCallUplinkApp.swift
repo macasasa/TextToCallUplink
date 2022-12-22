de059b40057043011564fa2addf54d79180e1dd0
macasasa <macasasa2009@gmail.com>
2022-12-22 21:20:39 +0200

    Initial Commit

---
+++ TextToCallUplink/TextToCallUplinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToCallUplinkApp.swift
+//  TextToCallUplink
+//
+//  Created by Nikolay on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TextToCallUplinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
